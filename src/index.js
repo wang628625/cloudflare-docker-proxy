017ea97d2d027398796175f331dd21393a86ae7a
wang628625 <38180013+wang628625@users.noreply.github.com>
2024-11-29 14:41:38 +0800

    Update index.js
    11

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + "dreamofsea.top"]: dockerHub,
+  ["quay." + "dreamofsea.top"]: "https://quay.io",
+  ["gcr." + "dreamofsea.top"]: "https://gcr.io",
+  ["k8s-gcr." + "dreamofsea.top"]: "https://k8s.gcr.io",
+  ["k8s." + "dreamofsea.top"]: "https://registry.k8s.io",
+  ["ghcr." + "dreamofsea.top"]: "https://ghcr.io",
+  ["cloudsmith." + "dreamofsea.top"]: "https://docker.cloudsmith.io",
+  ["ecr." + "dreamofsea.top"]: "https://public.ecr.aws",
 
   // staging
-  ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
+  ["docker-staging." + "dreamofsea.top"]: dockerHub,
 };
 
 function routeByHosts(host) {
